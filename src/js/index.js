36f855e3568490449e493523145b6e085b9ed191
Vítor Daniel Pontes Guedes <vitordaniel97.vtd@gmail.com>
2023-09-27 17:58:36 -0300

    bugfix: correção da função que verifica o resultado

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,19 @@ let judgeSpan = document.querySelector('#judge');
 const player1 = "X";
 const player2 = "O";
 let courrentPlayer = player1;
+const winResults = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9]
+];
+
 judgeSays(`Vez de ${courrentPlayer}`);
+
 
 function judgeSays(info) {
     judgeSpan.innerHTML = info;
@@ -15,23 +27,25 @@ function nextPlayer() {
         courrentPlayer = player2;
     }
     else courrentPlayer = player1;
-    judgeSays(`Vez de ${courrentPlayer}`)
-}
-
-// TODO: resolver problema de regra. [3, 5, 6, 7, 9] é um exemplo que está errado.
-function compareArrays(a, b) {
-    return JSON.stringify(a.sort().slice(0, 3)) === JSON.stringify(b)
+    judgeSays(`Vez de ${courrentPlayer}`);
 }
 
 
-function checkResult(list) {
-    for (let result of winResults) {
-        if (compareArrays(list, result)) {
-            judgeSays(`${courrentPlayer} ganhou!`);
+function compareArrays(list) {
+    for (let i of winResults) {
+        if (list.includes(i[0]) && list.includes(i[1]) && list.includes(i[2])) {
             return true;
         }
     }
     return false;
+}
+
+
+function checkResult() {
+    let list = courrentPlayer === player1 ? player1Plays : player2Plays;
+    if (!compareArrays(list)) return false;
+    judgeSays(`${courrentPlayer} venceu!`);
+    return true;
 }
 
 
@@ -42,22 +56,23 @@ function play(player, square, squarePosition) {
     if (player === player1) {
         playerList = player1Plays;
     }
-    else (playerList = player2Plays);
+    else {
+        playerList = player2Plays;
+    }
 
     playerList.push(squarePosition);
 
-    if (playerList.length >= 3) {
-        if(checkResult(playerList)) {
-            return;
-        }
+    if (checkResult()) {
+        return;
     }
+
     nextPlayer();
 }
 
 
 SQUARES.forEach(square => {
     square.addEventListener('click', () => {
-        if (!square.innerHTML) {
+        if (!square.innerHTML && !checkResult()) {
             play(courrentPlayer, square, parseInt(square.classList[1]))
         }
     });
@@ -65,14 +80,3 @@ SQUARES.forEach(square => {
 
 let player1Plays = [];
 let player2Plays = [];
-
-const winResults = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [1, 5, 9],
-    [3, 5, 7],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9]
-];
